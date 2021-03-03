0d71b0df8e39aa1086c7b6257624b3beb46e7d22
Irina-Nazarova <Irina.g.nazarova@yandex.ru>
2021-03-03 11:49:14 +0300

    update utils

--- recipe/utils.py
+++ recipe/utils.py
@@ -4,32 +4,32 @@ from django.shortcuts import get_object_or_404
 User = get_user_model()
 
 
-def generate_shop_list(request):
-    """ генерирует список покупок для выгрузки """
-
-    # для кого формируется список покупок
-    user = get_object_or_404(User, username=request.user.username)
-    # рецепты, ингридиенты которого составят список покупок
-    shop_list = user.purchases.all()
-    ingredients = {}  # словарь для списка покупок
-
-    for item in shop_list:
-        for elem in item.recipe.recipeingredient_set.all():
-        #for elem in item.recipe.all():
-
-            name = f'{elem.ingredient.name} ({elem.ingredient.unit})'
-            units = elem.amount
-
-            # суммирование кол-ва продуктов, если они дублируются
-            ingredients[name] = ingredients.get(name, 0) + units
-
-    # формирование из словаря удобоваримого списка на выгрузку
-    ingredients_download = []
-
-    for key, units in ingredients.items():
-        ingredients_download.append(f'{key} - {units}, ')
-
-    return ingredients_download
+# def generate_shop_list(request):
+#     """ генерирует список покупок для выгрузки """
+#
+#     # для кого формируется список покупок
+#     user = get_object_or_404(User, username=request.user.username)
+#     # рецепты, ингридиенты которого составят список покупок
+#     shop_list = user.purchases.all()
+#     ingredients = {}  # словарь для списка покупок
+#
+#     for item in shop_list:
+#         for elem in item.recipe.recipeingredient_set.all():
+#         #for elem in item.recipe.all():
+#
+#             name = f'{elem.ingredient.name} ({elem.ingredient.unit})'
+#             units = elem.amount
+#
+#             # суммирование кол-ва продуктов, если они дублируются
+#             ingredients[name] = ingredients.get(name, 0) + units
+#
+#     # формирование из словаря удобоваримого списка на выгрузку
+#     ingredients_download = []
+#
+#     for key, units in ingredients.items():
+#         ingredients_download.append(f'{key} - {units}, ')
+#
+#     return ingredients_download
 
 
 def get_ingredients(request):
